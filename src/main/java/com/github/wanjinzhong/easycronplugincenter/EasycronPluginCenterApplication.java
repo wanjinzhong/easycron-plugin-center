f98836ae523fd7c0bd87eaa54f6fa4b0710c45be
Neil Wan <neilw@synnex.com>
2019-04-26 10:53:55 +0800

    initial

---
+++ src/main/java/com/github/wanjinzhong/easycronplugincenter/EasycronPluginCenterApplication.java
@@ -0,0 +1,13 @@
+package com.github.wanjinzhong.easycronplugincenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasycronPluginCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasycronPluginCenterApplication.class, args);
+    }
+
+}
